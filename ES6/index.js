1ea633d7333656659a3add57d7340d7310e9ed9d
kiminkyu <34590448+koreadeveloper@users.noreply.github.com>
2019-01-20 20:38:48 +0900

    Add files via upload

---
+++ ES6/index.js
@@ -0,0 +1,21 @@
+let a = 221;
+let b = a-5;
+a= 4;
+console.log(b,a);
+
+const kiminkyu = {
+  name:"kiminkyu",
+  age:17,
+  gender:"Male",
+  isHandsome:true,
+  weight:60.0,
+  favoriteFood: ["kimchi","blgogi","pizza","chicken"]
+}
+
+console.log(kiminkyu.name);
+console.log(kiminkyu.age);
+console.log(kiminkyu.gender);
+console.log(kiminkyu.isHandsome);
+console.log(kiminkyu.weight);
+kiminkyu.weight = 60.1;
+console.log(kiminkyu.weight);
