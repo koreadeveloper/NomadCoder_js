98be54ffc40f13aa5497b30c42dde76b2d96660e
kiminkyu <34590448+koreadeveloper@users.noreply.github.com>
2019-02-08 10:16:57 +0900

    2019.02.08
    Project 진행중

---
+++ nomadProject/clock.js
@@ -0,0 +1,7 @@
+const clockContainer = document.querySelector(".js-clock");
+  clockTitle = clockContainer.querySelector("hi";)
+function init(){
+
+}
+
+init();
---
+++ nomadcoder_js_practice/index (2).js
@@ -0,0 +1,22 @@
+//---------------------------------------------------------
+/*
+const BASE_COLOR = "#dddddd";
+const OTHER_COLOR= "#7f8c8d";
+
+function handleClick(){
+  const currentColor = title.style.color;
+  if(currentColor === BASE_COLOR){
+    title.style.color = OTHER_COLOR;
+  } else{
+    title.style.color = BASE_COLOR;
+  }
+}
+function init(){
+  title.style.color=BASE_COLOR;
+  title.addEventListner("click",handleClick);
+}
+init();
+*/
+//---------------------------------------------------------
+//위 코드는 에러나니까 주의
+//하다가 귀찮아서 포기
